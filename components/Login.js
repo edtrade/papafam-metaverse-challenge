0c234a6a484fb466460e12f4f295a8a24cb015f3
edtrade <edtrade88@gmail.com>
2021-12-16 18:33:47 -0500

    day2

---
+++ components/Login.js
@@ -0,0 +1,23 @@
+import Image from "next/image"
+function Login({authenticate}) {
+    return (
+        <div className="bg-black relative">
+            Login Component
+            <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+                <Image className="object-cover rounded-full"
+                    src="https://links.papareact.com/3pi" height={200} width={200}/>
+                <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+                    onClick={() => authenticate()}>
+                    Login with Meta Mask
+                </button>
+            </div>
+
+            <div className="w-full h-screen">
+                <Image src="https://links.papareact.com/55n" layout="fill" objectFit="cover"/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
